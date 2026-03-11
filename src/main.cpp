953b1893b4871b9fa7e0ff1b4323e5bae7fdb241
ultimatehecker <86735991+ultimatehecker@users.noreply.github.com>
2026-03-11 12:21:15 -0400

    added dht code

--- src/main.cpp
+++ src/main.cpp
@@ -1,76 +1,75 @@
-
-#include <dhtnew.h>
 #include <Arduino.h>
+#include <dhtnew.h>
 
-DHTNEW mySensor(5);   //  ESP 16    UNO 5    MKR1010 5
+DHTNEW mySensor(5);
 
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);      // set LED pin as output
+  digitalWrite(LED_BUILTIN, LOW);    // switch off LED pin
 
-void setup()
-{
-  while (!Serial);    //  MKR1010 needs this
-
-  Serial.begin(115200);
-  Serial.println();
-  Serial.println("dhtnew_dht22.ino");
+  Serial1.begin(9600);            // initialize UART with baud rate of 9600
+  Serial1.println();
   Serial.print("DHTNEW_LIB_VERSION: ");
   Serial.println(DHTNEW_LIB_VERSION);
   Serial.println();
-
-  //  MKR1010
-  //  mySensor.setDisableIRQ(false);
-
-  Serial.println("\n1. Type detection test, first run might take longer to determine type");
-  Serial.println("STAT\tHUMI\tTEMP\tTYPE");
-
-  mySensor.setType(22);
-
 }
-
-
-void loop()
-{
-  int chk = mySensor.read();
-  switch (chk)
-  {
-  case DHTLIB_OK:
-    Serial.print("OK,\t");
-    break;
-  case DHTLIB_ERROR_CHECKSUM:
-    Serial.print("Checksum error,\t");
-    break;
-  case DHTLIB_ERROR_TIMEOUT_A:
-    Serial.print("Time out A error,\t");
-    break;
-  case DHTLIB_ERROR_TIMEOUT_B:
-    Serial.print("Time out B error,\t");
-    break;
-  case DHTLIB_ERROR_TIMEOUT_C:
-    Serial.print("Time out C error,\t");
-    break;
-  case DHTLIB_ERROR_TIMEOUT_D:
-    Serial.print("Time out D error,\t");
-    break;
-  case DHTLIB_ERROR_SENSOR_NOT_READY:
-    Serial.print("Sensor not ready,\t");
-    break;
-  case DHTLIB_ERROR_BIT_SHIFT:
-    Serial.print("Bit shift error,\t");
-    break;
-  case DHTLIB_WAITING_FOR_READ:
-    Serial.print("Waiting for read,\t");
-    break;
-  default:
-    Serial.print("Unknown: ");
-    Serial.print(chk);
-    Serial.print(",\t");
-    break;
+void loop() {
+  while (Serial1.available() >= 0) {
+    char receivedData = Serial1.read();   // read one byte from serial buffer and save to receivedData
+    if (receivedData == '1') {
+      digitalWrite(LED_BUILTIN, HIGH); // switch LED On
+    }
+    else if (receivedData == '0') {
+      digitalWrite(LED_BUILTIN, LOW);  // switch LED Off
+    }
   }
 
-  //  DISPLAY DATA
-  Serial.print(mySensor.getHumidity(), 1);
-  Serial.print(",\t");
-  Serial.print(mySensor.getTemperature(), 1);
-  Serial.print(",\t");
-  Serial.println(mySensor.getType());
-  delay(2000);
+  uint32_t start = micros();
+  int chk = mySensor.read();
+  uint32_t stop = micros();
+
+  if (chk != DHTLIB_WAITING_FOR_READ) {
+    switch (chk) {
+      case DHTLIB_OK:
+        Serial.print("OK,\t");
+        break;
+      case DHTLIB_ERROR_CHECKSUM:
+        Serial.print("Checksum error,\t");
+        break;
+      case DHTLIB_ERROR_TIMEOUT_A:
+        Serial.print("Time out A error,\t");
+        break;
+      case DHTLIB_ERROR_TIMEOUT_B:
+        Serial.print("Time out B error,\t");
+        break;
+      case DHTLIB_ERROR_TIMEOUT_C:
+        Serial.print("Time out C error,\t");
+        break;
+      case DHTLIB_ERROR_TIMEOUT_D:
+        Serial.print("Time out D error,\t");
+        break;
+      case DHTLIB_ERROR_SENSOR_NOT_READY:
+        Serial.print("Sensor not ready,\t");
+        break;
+      case DHTLIB_ERROR_BIT_SHIFT:
+        Serial.print("Bit shift error,\t");
+        break;
+      default:
+        Serial.print("Unknown: ");
+        Serial.print(chk);
+        Serial.print(",\t");
+        break;
+    }
+
+    Serial.print(mySensor.getHumidity(), 1);
+    Serial.print(",\t");
+    Serial.print(mySensor.getTemperature(), 1);
+    Serial.print(",\t");
+
+    uint32_t duration = stop - start;
+
+    Serial.print(duration);
+    Serial.print(",\t");
+    Serial.println(mySensor.getType());
+  }
 }
